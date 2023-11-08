3b72a075629d6118afaf6524054f9e1aca5a84d7
josiahfs <josiahfrlswt@gmail.com>
2023-11-09 03:07:32 +0700

    edit Image

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import Button from "./Button";
-import Image from "next/Image";
+import Image from "next/image";
 
 const ContactSection = () => {
   return (
