cf613f6f765fba709cbcf61281f88da25f80d5b4
Josiah Farrel Suwito <josiahfs@Josiahs-Laptop.local>
2024-04-08 07:30:47 +0700

    fix tag fetch

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 const PortfolioCard = ({ portfolio }) => {
-  const { title, heading, description, image, slug } = portfolio.fields;
+  const { title, heading, description, image, slug, tag, date } =
+    portfolio.fields;
 
   return (
     <div className="w-fill h-fill bg-[#1D1C1B] rounded-xl">
@@ -16,17 +17,21 @@ const PortfolioCard = ({ portfolio }) => {
       <div className="flex flex-col px-[28px] my-[24px] overflow-hidden w-full space-y-1">
         <div className="flex justify-between items-center">
           <div className="flex space-x-2">
-            <p className="text-[#0B0B0B] font-medium text-[10px] bg-[#C19C63] px-[8px] py-[4px] rounded-lg">
-              Team Project
-            </p>
-            <p className="text-[#0B0B0B] font-medium text-[10px] bg-[#C19C63] px-[8px] py-[4px] rounded-lg">
-              Mobile App
-            </p>
+            {tag.map((tag) => (
+              <p
+                className="text-[#0B0B0B] font-medium text-[10px] bg-[#C19C63] px-[8px] py-[4px] rounded-lg"
+                key={tag}
+              >
+                {tag}
+              </p>
+            ))}
           </div>
-          <p className="text-[#C19C63] font-medium text-xs">2023</p>
         </div>
         <h1 className="text-xl text-white font-bold">{title}</h1>
-        <h3 className="text-[#C19C63] font-medium text-sm">{heading}</h3>
+        <div className="flex justify-between">
+          <h3 className="text-[#C19C63] font-medium text-sm">{heading}</h3>
+          <p className="text-[#C19C63] font-medium text-xs">{date}</p>
+        </div>
         <div className="h-[90px] overflow-scroll">
           <p className="text-[#B1B0B0] font-medium text-xs text-justify">
             {description}
