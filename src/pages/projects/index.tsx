44160371d5def8cbe9122e6753edaa055fe8de82
Josiah Farrel Suwito <josiahfs@Josiahs-Laptop.local>
2024-04-20 02:28:57 +0700

    fix fetch issue

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -8,10 +8,17 @@ import "../../app/globals.css";
 import { createClient } from "contentful";
 
 export async function getStaticProps() {
+  const space = process.env.CONTENTFUL_SPACE_ID || "";
+  const accessToken = process.env.CONTENTFUL_ACCESS_KEY || "";
+
   const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    space,
+    accessToken,
   });
+  // const client = createClient({
+  //   space: process.env.CONTENTFUL_SPACE_ID,
+  //   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  // });
 
   const res = await client.getEntries({ content_type: "portfolio" });
 
